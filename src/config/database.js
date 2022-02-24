d0f06212dc13b2db42d05ecd01ade2dbad455397
Jefferson <jeffersonsarti@gmail.com>
2022-02-24 01:35:13 -0300

    fix: removed unusued env var

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,3 @@
-const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_CONN_STRING } = process.env;
+const { DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 
-module.exports = DB_CONN_STRING.replace("<username>", DB_USERNAME)
-  .replace("<password>", DB_PASSWORD)
-  .replace("<dbname>", DB_NAME);
+module.exports = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster-sete-espadas.pqcfu.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
