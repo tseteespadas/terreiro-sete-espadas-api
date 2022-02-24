6fbee51004ecf943f7b60ab1f99b4f388f9dfcd4
Jefferson <jeffersonsarti@gmail.com>
2022-02-24 02:44:19 -0300

    fix: fixed app listening to heroku port

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 require("dotenv").config(); // setting up the env vars
-const appPort = require("./config/app");
 const express = require("express");
 const cors = require("cors");
 
@@ -10,4 +9,4 @@ app.use(express.json());
 require("./controllers/giraController")(app);
 require("./controllers/giraRegistrationController")(app);
 
-app.listen(appPort || 5000);
+app.listen(process.env.PORT || 8080)
