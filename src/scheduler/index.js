94f25e503f7e70ae42c72d24b16f5417cc113842
Terreiro Sete Espadas <85307952+tseteespadas@users.noreply.github.com>
2023-11-25 16:25:06 -0300

    Update index.js

--- src/scheduler/index.js
+++ src/scheduler/index.js
@@ -11,7 +11,7 @@ const cron = require("node-cron");
 # │ │ │ │ │ │
 # * * * * * *
 */
-const BILLING_SCHEDULER = "20 19 25 * *";
+const BILLING_SCHEDULER = "30 19 25 * *";
 
 const billingScheduler = require("./billingScheduler");
 
